6a0dd7590c143e54e39014a8d4e6a4ee84493282
Maxine Otten <maxine.otten@student.pxl.be>
2026-01-10 18:18:54 +0100

    realisaties.html toegevoegd

--- stage.js
+++ stage.js
@@ -1,26 +1,40 @@
-const track = document.getElementById("track");
-const images = document.querySelectorAll(".carousel-track img");
+function setupCarousel(trackId) {
+    const track = document.getElementById(trackId);
+    const carousel = track.parentElement;
+    const images = track.querySelectorAll("img");
 
-const imageWidth = 205; // foto + margin
-let position = 0;
+    const imageWidth = images[0].offsetWidth + 5;
+    const visibleImages = Math.floor(carousel.offsetWidth / imageWidth);
 
-// max verschuiving zodat er altijd 3 zichtbaar blijven
-const maxPosition = -(imageWidth * (images.length - 3));
+    let position = 0;
+    const maxPosition = -(imageWidth * (images.length - visibleImages));
 
-function volgende() {
-    if (position > maxPosition) {
-        position -= imageWidth;
-    } else {
-        position = 0; // terug naar begin
-    }
-    track.style.transform = `translateX(${position}px)`;
+
+    return {
+        volgende() {
+            console.log(position > maxPosition);
+            if (position > maxPosition) {
+                position -= imageWidth;
+            } else {
+                position = 0;
+            }
+            track.style.transform = `translateX(${position}px)`;
+        },
+        vorige() {
+            if (position < 0) {
+                position += imageWidth;
+            } else {
+                position = maxPosition;
+            }
+            track.style.transform = `translateX(${position}px)`;
+        }
+    };
 }
 
-function vorige() {
-    if (position < 0) {
-        position += imageWidth;
-    } else {
-        position = maxPosition; // naar einde
-    }
-    track.style.transform = `translateX(${position}px)`;
-}
+let budapestCarousel;
+let tanzaniaCarousel;
+
+window.addEventListener('load', () => {
+     budapestCarousel = setupCarousel("b-track");
+     tanzaniaCarousel = setupCarousel("t-track");
+});
